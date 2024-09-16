b7ce62c23ad1cd3539ee8c40990aa775f07daa79
mrdog233o5 <mr.dog233o5@gmail.com>
2024-09-16 22:24:46 +0800

    set up prettier

--- api/index.ts
+++ api/index.ts
@@ -4,11 +4,12 @@ const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+	res.send("Hello World!");
+	console.log(res.headersSent);
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+	console.log(`Listening on port ${port}...`);
 });
 
 module.exports = app;
