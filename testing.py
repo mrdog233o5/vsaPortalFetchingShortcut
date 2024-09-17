da7ff4b9f9dc3234ef2869413b6e2da42ece3a13
mrdog233o5 <mr.dog233o5@gmail.com>
2024-09-17 14:47:06 +0800

    changed dir

--- testing.py
+++ testing.py
@@ -2,6 +2,7 @@ from datetime import date
 import requests
 
 url = 'http://localhost:3000/'
+url = 'https://vsa-portal-fetching-shortcut.vercel.app/'
 x = requests.get(url, headers = {"date":f"{str(date.today())}"})
 
 print(x.text)
